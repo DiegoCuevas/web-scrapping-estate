1321b588f6d76b69b2618d193d78ae3cb151130a
Diego Cuevas <diegocuevas579@gmail.com>
2022-01-26 17:41:14 -0500

    ADD: some attributes to json

--- property.rb
+++ property.rb
@@ -7,9 +7,18 @@ class Property
   def initialize(url)
     @url = url
     @html = Nokogiri::HTML(open(url))
+    @data = JSON.parse(File.read("data/properties.json"))
+    @hash = Hash.new
   end
   def get_property_info
-    
+    @html.css("ul.listado-viviendas").children.to_a.each do |li|
+      @hash[:title] = li.css('h2.title').text.strip
+      @hash[:price] = li.css('p.price').text
+      @data["data"].push(@hash)
+    end
+    File.open("data/properties.json","w") do |f|
+      f.write(@data.to_json)
+    end
   end
 end 
 
