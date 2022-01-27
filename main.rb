ae3e8a745d949f323735965c5fdd82401958af48
Diego Cuevas <diegocuevas579@gmail.com>
2022-01-27 13:59:26 -0500

    ADD: script to get properties

--- main.rb
+++ main.rb
@@ -1,5 +1,13 @@
 require_relative 'property'
 
-properties = Property.new("https://www.laencontre.com.pe/agente/palo-alto-inmobiliaria-37195/venta/propiedades")
-properties.get_property_info
-properties.generate_json
+
+# properties = Property.new("https://www.laencontre.com.pe/agente/palo-alto-inmobiliaria-37195/venta/t-departamentos")
+puts "ingrese una url"
+url = gets.chomp
+until url.match?(/^(http|https):\/\/|[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$/)
+  puts 'Ingrese una url valida'
+  response = gets.chomp
+end
+property = Property.new(url)
+property.generate_json
+puts "json generado"
