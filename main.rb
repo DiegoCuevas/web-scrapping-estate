99b2917423f398ae289368b05ed4bf7ea8340f52
Diego Cuevas <diegocuevas579@gmail.com>
2022-01-27 12:35:09 -0500

    ADD: generate json file

--- main.rb
+++ main.rb
@@ -2,3 +2,4 @@ require_relative 'property'
 
 properties = Property.new("https://www.laencontre.com.pe/agente/palo-alto-inmobiliaria-37195/venta/propiedades")
 properties.get_property_info
+properties.generate_json
